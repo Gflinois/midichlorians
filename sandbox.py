fd8369a54f53e0b1b96919db0175b45402e1d8b6
Gflinois <guillaume.flinois@ens-paris-saclay.fr>
2024-02-28 15:28:11 +0100

    info about the pack determiner

--- sandbox.py
+++ sandbox.py
@@ -21,3 +21,39 @@ dlim=d1[:1000,:]
 print(dlim.size())
 print(nnc2(dlim).size())
 """
+
+
+m1 = dd["CLA"][0]["marker"]
+n=0
+lh=0
+rh=0
+o=0
+IRP=0
+ISRP=0
+EE=0
+for i in m1 :
+	n+=i[0][0]
+	lh+=i[0][1]
+	rh+=i[0][2]
+	o+=i[0][3]
+	IRP+=i[0][4]
+	ISRP+=i[0][5]
+	EE+=i[0][6]
+
+print("repartition :\n\tnothing = ",n/m1.size()[0]*100,"\n\tLH = ",lh/m1.size()[0]*100,"\n\tRH = ",rh/m1.size()[0]*100,"\n\tO = ",o/m1.size()[0]*100,"\n\tIRP = ",IRP/m1.size()[0]*100,"\n\tISRP = ",ISRP/m1.size()[0]*100,"\n\tEE = ",EE/m1.size()[0]*100)		
+
+
+i=0
+
+while not m1[i][0][3]:
+	i+=1	
+
+start=i
+while m1[i][0][3]:
+	i+=1
+
+length = i-start
+		
+print("length of a O = ",length)		
+
+
