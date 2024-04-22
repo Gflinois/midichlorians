548b775af1f148bc0501f8509a978ce534a4fc62
Gflinois <guillaume.flinois@ens-paris-saclay.fr>
2024-04-22 09:15:47 +0200

    forgot lol

--- sichao/heat.m
+++ sichao/heat.m
@@ -23,16 +23,18 @@ fs = 250; %maybe sampling frequency?
 framewidth = 30;
 wave = "amor";%using morlet waves
 signals = filter_banks(sensor).(fields{f});
+sig = signals{signal};
 
 %% morlet transform of the data
-[y, hz] = get_cwt_img(signals{signal}, wave, fs);% 
+[y, hz] = get_cwt_img(sig, wave, fs);% 
 limits= [round(min(min(y(1:30,:)))) round(max(max(y(1:30,:))))];
 
 
 
 
 %% heatmap plotting
-for currentframe = 1:250-framewidth
+%for currentframe = 1:250-framewidth
+for currentframe = 1:240
 y_limited = y(1:30,:);
 current_y = y_limited(:,currentframe:currentframe+30);
 h = heatmap(current_y);
